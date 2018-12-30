16a52b59dba021a55fea8e96d8fd1bd2593864df
andrew-vicencio <andrew.v1124@gmail.com>
2018-12-29 20:48:39 -0500

    Finished v1. Added styling and post routing for new campgrounds.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 var campgrounds = 
 [
@@ -8,16 +13,24 @@ var campgrounds =
     {name: 'algonquin', image: 'https://upload.wikimedia.org/wikipedia/commons/3/33/ON_-_Algonquin_Provincial_Park.jpg'},
 ];
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-
 app.get('/', function(req, res){
     res.render("landing");
 });
 
 app.get('/campgrounds', function(req, res){
     res.render("campgrounds", {camps: campgrounds});
+});
+
+app.get('/campgrounds/new', function(req, res){
+    res.render("new");
 })
+
+app.post('/campgrounds', function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    campgrounds.push({name: name, image: image});
+    res.redirect('/campgrounds');
+});
 
 app.listen(3000, function(){
     console.log('YelpCamp started on port 3000');
