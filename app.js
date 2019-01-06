5fb52e354e7762e2b02100f5ecf7168b282aea38
andrew-vicencio <andrew.v1124@gmail.com>
2019-01-06 11:00:31 -0500

    Added styling to show page.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express         = require('express'),
 
 mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 // seedDB();
 
