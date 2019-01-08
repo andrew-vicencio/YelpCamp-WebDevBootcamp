102c1805944c00541af284a72d4ead982c414724
andrew-vicencio <andrew.v1124@gmail.com>
2019-01-07 23:49:52 -0500

    Added environment variables for db and port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const commentRoutes = require('./routes/comments'),
 var app = express();
 
 // mongoose config and seeding
-mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true });
+mongoose.connect(`${process.env.DB_URL}`, { useNewUrlParser: true });
 mongoose.set('useFindAndModify', false);
 // seedDB();
 
@@ -53,6 +53,6 @@ app.use('/', indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
     console.log('YelpCamp started on port 3000');
 });
