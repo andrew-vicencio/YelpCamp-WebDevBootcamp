1ac4fc3d836f50f117b005dd41178bc274e135f2
andrew-vicencio <andrew.v1124@gmail.com>
2019-03-06 23:02:35 -0500

    Reverted to ejs app. Added better port handling.

--- app.js
+++ app.js
@@ -53,6 +53,8 @@ app.use('/', indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-app.listen(process.env.PORT, function () {
-    console.log('YelpCamp started on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log(`YelpCamp started on port ${PORT}`);
 });
